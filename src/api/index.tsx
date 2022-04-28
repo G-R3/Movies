12a6b65d206b70c1ff9403a5eeb6e595789fc3b3
Gerardo <gr261212@gmail.com>
2022-04-28 00:37:58 -0400

    fetch a movie

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,8 +1,8 @@
 const API_KEY: string = "f7e5346f3dbaa2b500da070bdda05af4";
-
+const BASE_URL: string = "https://api.themoviedb.org/3/";
 const getMovies = async () => {
     const response = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+        `${BASE_URL}discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
     );
 
     const data = await response.json();
@@ -10,5 +10,14 @@ const getMovies = async () => {
     return data;
 };
 
+const getMovie = async (movieId: string) => {
+    const response = await fetch(
+        `${BASE_URL}movie/${movieId}?api_key=${API_KEY}&language=en-US`
+    );
+    const data = await response.json();
+
+    return data;
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { getMovies };
+export { getMovies, getMovie };
