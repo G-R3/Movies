abb275282dbd3d05cf9d024de943e94ca38b82a6
Gerardo <gr261212@gmail.com>
2022-04-28 12:54:32 -0400

    add chakra component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,20 @@
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+
 const Home = (): JSX.Element => {
     return (
-        <main className="text-center h-64 flex flex-col items-center justify-center gap-5">
-            <h1 className="text-4xl">Movie Manager</h1>
-            <p className="text-base">Track and organize your movies</p>
-            <button
-                type="button"
-                className="bg-violet-400 text-white p-2 rounded-md"
-            >
+        <Flex
+            as="main"
+            direction="column"
+            alignItems="center"
+            justify="center"
+            h="300px"
+        >
+            <Heading fontSize="4xl">Movie Manager</Heading>
+            <Text>Track and organize your movies</Text>
+            <Button mt="5" colorScheme="purple">
                 Get Started
-            </button>
-        </main>
+            </Button>
+        </Flex>
     );
 };
 
