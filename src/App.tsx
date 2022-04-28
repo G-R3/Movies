ba148cef720ddc21b1097ea90addf2fc40ebe309
Gerardo <gr261212@gmail.com>
2022-04-27 23:37:04 -0400

    add and setup reat router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import { Home, Browse } from "./pages";
+
 function App(): JSX.Element {
-    return <></>;
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="browse" element={<Browse />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
