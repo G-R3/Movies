3ffa292c4037d7b95d1d1da77a4ae653c602d186
Gerardo <gr261212@gmail.com>
2022-05-04 20:47:36 -0400

    format runtime, votes, and budget values

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -11,23 +11,32 @@ import {
     Stat,
     StatLabel,
     StatNumber,
+    HStack,
 } from "@chakra-ui/react";
 import { getMovie } from "../api";
 import GenreTag from "../components/Tag";
+import { numberFormat, minutesToHrsMins } from "../utils/index";
 
 interface Genres {
     id: number;
     name: string;
 }
 interface Movie {
-    title: string;
+    adult: false;
     backdrop_path: string;
+    budget: number;
     genres: Genres[];
-    tagline: string;
+    id: number;
+    original_title: string;
     overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    runtime: number;
+    title: string;
+    tagline: string;
     vote_average: number;
     vote_count: number;
-    budget: number;
 }
 
 const MoviePage = () => {
@@ -37,6 +46,7 @@ const MoviePage = () => {
     useEffect(() => {
         const fetchData = async () => {
             const data = await getMovie(movieId!);
+            console.log(data);
             setMovie(data);
         };
 
@@ -46,22 +56,27 @@ const MoviePage = () => {
     if (!movie) return <p>Loading..</p>;
     return (
         <Flex direction="column" justifyContent="space-between" h="full">
-            <SimpleGrid columns={{ sm: 1, md: 2 }} spacingX="40px" mt={10}>
+            <SimpleGrid
+                columns={{ sm: 1, md: 2 }}
+                spacingX="40px"
+                mt={10}
+                alignItems="center"
+            >
                 <Box>
                     <Image
-                        borderRadius="md"
+                        borderRadius="lg"
                         src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
                         alt={movie.title}
                         shadow="2xl"
                     />
                 </Box>
-                <Box>
+                <Flex direction="column">
                     <Flex direction="row" gap={3} mt={3}>
                         {movie.genres.map((genre) => (
                             <GenreTag key={genre.id} name={genre.name} />
                         ))}
                     </Flex>
-                    <Flex direction="column" gap={1}>
+                    <Flex direction="column" gap={1} flex="1 0 0">
                         <Heading
                             as="h1"
                             fontSize={{
@@ -78,7 +93,33 @@ const MoviePage = () => {
                         </Text>
                         <Text mt={2}>{movie.overview}</Text>
                     </Flex>
-                </Box>
+                    <HStack mt={10} textAlign="center">
+                        <Stat>
+                            <StatLabel
+                                color="gray.500"
+                                fontSize="md"
+                                fontWeight="normal"
+                            >
+                                Release Date
+                            </StatLabel>
+                            <StatNumber fontSize="sm" fontWeight="bold">
+                                {movie.release_date}
+                            </StatNumber>
+                        </Stat>
+                        <Stat>
+                            <StatLabel
+                                color="gray.500"
+                                fontSize="md"
+                                fontWeight="normal"
+                            >
+                                Runtime
+                            </StatLabel>
+                            <StatNumber fontSize="sm" fontWeight="bold">
+                                {minutesToHrsMins(movie.runtime)}
+                            </StatNumber>
+                        </Stat>
+                    </HStack>
+                </Flex>
             </SimpleGrid>
             <Flex
                 direction={{ base: "column", md: "row" }}
@@ -90,6 +131,7 @@ const MoviePage = () => {
                         fontSize="md"
                         textAlign="center"
                         color="gray.500"
+                        fontWeight="normal"
                     >
                         Rating
                     </StatLabel>
@@ -110,6 +152,7 @@ const MoviePage = () => {
                         fontSize="md"
                         textAlign="center"
                         color="gray.500"
+                        fontWeight="normal"
                     >
                         Vote Count
                     </StatLabel>
@@ -118,7 +161,7 @@ const MoviePage = () => {
                         fontWeight="bold"
                         textAlign="center"
                     >
-                        {movie.vote_count}{" "}
+                        {numberFormat(movie.vote_count)}{" "}
                         <chakra.span fontSize="2xl" color="gray.500">
                             {" "}
                             votes
@@ -130,6 +173,7 @@ const MoviePage = () => {
                         fontSize="md"
                         textAlign="center"
                         color="gray.500"
+                        fontWeight="normal"
                     >
                         Budget
                     </StatLabel>
@@ -138,11 +182,9 @@ const MoviePage = () => {
                         fontWeight="bold"
                         textAlign="center"
                     >
-                        ${movie.budget}{" "}
-                        <chakra.span fontSize="2xl" color="gray.500">
-                            {" "}
-                            usd
-                        </chakra.span>
+                        {movie.budget !== 0
+                            ? `$${numberFormat(movie.budget)}`
+                            : "N/A"}
                     </StatNumber>
                 </Stat>
             </Flex>
