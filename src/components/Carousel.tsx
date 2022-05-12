adfcef698ea6e29c2a9c2f6d6c4cc06cd3e50b5b
Gerardo <gr261212@gmail.com>
2022-05-12 01:34:24 -0400

    add heading prop

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,39 +1,53 @@
 /* eslint-disable import/no-unresolved */
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
+import { Box, Heading } from "@chakra-ui/react";
 import Card from "./Card";
 
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/navigation";
 
-export default function Carousel({ data }: any): JSX.Element {
+export default function Carousel({ data, heading }: any): JSX.Element {
     return (
-        <Swiper
-            slidesPerView={3}
-            spaceBetween={10}
-            navigation
-            freeMode
-            modules={[Navigation]}
-            wrapperTag="ul"
-            breakpoints={{
-                320: {
-                    slidesPerView: 1,
-                },
-                480: {
-                    slidesPerView: 3,
-                },
-            }}
-        >
-            {data.map((movie: any) => (
-                <SwiperSlide
-                    key={movie.id}
-                    tag="li"
-                    style={{ listStyle: "none" }}
-                >
-                    <Card movie={movie} />
-                </SwiperSlide>
-            ))}
-        </Swiper>
+        <Box>
+            <Heading
+                as="h2"
+                mb={4}
+                fontSize={{
+                    base: "xl",
+                    md: "2xl",
+                    lg: "3xl",
+                }}
+            >
+                {heading}
+            </Heading>
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={20}
+                navigation
+                freeMode
+                modules={[Navigation]}
+                wrapperTag="ul"
+                breakpoints={{
+                    320: {
+                        slidesPerView: 1,
+                    },
+                    480: {
+                        slidesPerView: 3,
+                    },
+                }}
+            >
+                {data.map((movie: any) => (
+                    <SwiperSlide
+                        key={movie.id}
+                        tag="li"
+                        style={{ listStyle: "none" }}
+                    >
+                        <Card movie={movie} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </Box>
     );
 }
