42b0336af0d5a7960e53ed5f280e589dea01e7bd
Gerardo <gr261212@gmail.com>
2022-05-14 02:01:48 -0400

    update port

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,14 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/api/", (req: Request, res: Response) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.post("/api/register", (req: Request, res: Response) => {
+    console.log(req.body);
     return res.send({ message: "Hello from server" });
 });
 
-app.listen(3001, () => {
-    console.log("listening on PORT 3000");
+app.listen(5000, () => {
+    console.log("listening on PORT 5000");
 });
