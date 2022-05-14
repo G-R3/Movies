7088e6704f266e740cdb442f49f9abd0bce0b182
Gerardo <gr261212@gmail.com>
2022-05-14 15:04:24 -0400

    changed some padding

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -6,8 +6,8 @@ const Layout = (): JSX.Element => {
     const bg = useColorModeValue("gray.100", "gray.900");
     const color = useColorModeValue("gray.800", "gray.300");
     return (
-        <Box backgroundColor={bg} color={color} h="full">
-            <Container maxW="container.xl">
+        <Box backgroundColor={bg} color={color} h="full" paddingX={[2, 10, 20]}>
+            <Container maxW="container.xl" p={0}>
                 <header>
                     <Navbar />
                 </header>
