64d1d346c3c07d36bf8e34929bf0fa4b40522129
Gerardo <gr261212@gmail.com>
2022-05-16 13:19:34 -0400

    create useAuthForm

--- client/src/components/AuthForm.tsx
+++ client/src/components/AuthForm.tsx
@@ -14,35 +14,7 @@ import {
     ModalCloseButton,
     HStack,
 } from "@chakra-ui/react";
-import { ChangeEvent, FormEvent, useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-type ErrorData = {
-    email?: string;
-    password?: string;
-};
-type FormData = {
-    email: string;
-    password: string;
-};
-
-const validate = (values: FormData): ErrorData => {
-    let errors: ErrorData = {};
-
-    if (!values.email) {
-        errors.email = "Required";
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = "Invalid email address";
-    }
-
-    if (!values.password) {
-        errors.password = "Required";
-    } else if (values.password.length < 6) {
-        errors.password = "Password must be at least 6 characters long";
-    }
-
-    return errors;
-};
+import useAuthForm from "../hooks/useAuthForm";
 
 type Props = {
     showSignUpForm: boolean;
@@ -57,74 +29,15 @@ const AuthForm = ({
     isOpen,
     onClose,
 }: Props): JSX.Element => {
-    const [formData, setFormData] = useState<FormData>({
-        email: "",
-        password: "",
-    });
-    const [errors, setErrors] = useState<ErrorData>({});
+    let url: string;
+    if (showSignUpForm) {
+        url = "/api/register";
+    } else {
+        url = "/api/login";
+    }
 
-    const navigate = useNavigate();
-
-    const handleSubmit = async (
-        e: FormEvent<HTMLFormElement>
-    ): Promise<void> => {
-        e.preventDefault();
-
-        const validationErrors = validate(formData);
-
-        if (Object.keys(validationErrors).length !== 0) {
-            setErrors(validationErrors);
-            return;
-        }
-
-        let url;
-        if (showSignUpForm) {
-            url = "/api/register";
-        } else {
-            url = "/api/login";
-        }
-
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: formData.email,
-                    password: formData.password,
-                }),
-            });
-
-            const data = await response.json();
-
-            if (data.status === 400) {
-                console.log("Form field errors");
-                console.log(data);
-                return;
-            }
-            console.log(data);
-
-            setErrors({});
-            setFormData({
-                email: "",
-                password: "",
-            });
-            onClose();
-
-            navigate("/browse");
-        } catch (err) {
-            console.log("ERROR SUBMITTING");
-            console.log(err);
-        }
-    };
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
-        setFormData((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value,
-        }));
-    };
+    const { handleSubmit, handleChange, formData, errors, isSubmitting } =
+        useAuthForm({ email: "", password: "", url, onClose });
 
     const { email, password } = formData;
 
@@ -142,7 +55,12 @@ const AuthForm = ({
                 </ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    <form id="register-form" onSubmit={(e) => handleSubmit(e)}>
+                    <form
+                        id="register-form"
+                        onSubmit={(e) => {
+                            handleSubmit(e);
+                        }}
+                    >
                         <FormControl isInvalid={errors.hasOwnProperty("email")}>
                             <FormLabel htmlFor="email">Email address</FormLabel>
                             <Input
@@ -207,6 +125,7 @@ const AuthForm = ({
                             type="submit"
                             colorScheme="blue"
                             form="register-form"
+                            isLoading={isSubmitting}
                         >
                             Submit
                         </Button>
---
+++ client/src/hooks/useAuthForm.ts
@@ -0,0 +1,115 @@
+import { ChangeEvent, FormEvent, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+type ErrorData = {
+    email?: string;
+    password?: string;
+};
+type Props = {
+    email?: string;
+    password?: string;
+    url: string;
+    onClose: any;
+};
+type FormData = {
+    email?: string;
+    password?: string;
+};
+
+const useAuthForm = ({ email, password, url, onClose }: Props) => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState<FormData>({
+        email: email,
+        password: password,
+    });
+    const [errors, setErrors] = useState<ErrorData>({});
+    const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isSubmitting) {
+            const hasErrors = Object.keys(errors).length !== 0;
+            if (!hasErrors) {
+                try {
+                    const authUser = async (url: string) => {
+                        const response = await fetch(url, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify({
+                                email: formData.email,
+                                password: formData.password,
+                            }),
+                        });
+
+                        const data = await response.json();
+                        if (!data.success) {
+                            throw data.message;
+                        }
+
+                        setErrors({});
+                        setFormData({
+                            email: "",
+                            password: "",
+                        });
+                        setIsSubmitting(false);
+                        onClose();
+                        console.log("Authenticated");
+                        navigate("/");
+                    };
+
+                    authUser(url).catch((err) => {
+                        console.log(err);
+                        setIsSubmitting(false);
+                    });
+                } catch (err) {
+                    console.log("Error submitting form");
+                    console.log(err);
+                    setIsSubmitting(false);
+                }
+            } else {
+                setIsSubmitting(false);
+            }
+        }
+    }, [errors]);
+
+    const validate = (values: FormData): ErrorData => {
+        let errors: ErrorData = {};
+
+        if (!values.email) {
+            errors.email = "Required";
+        } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+            errors.email = "Invalid email address";
+        }
+
+        if (!values.password) {
+            errors.password = "Required";
+        } else if (values.password.length < 6) {
+            errors.password = "Password must be at least 6 characters long";
+        }
+
+        return errors;
+    };
+
+    const handleSubmit = async (
+        e: FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+        e.preventDefault();
+        const validationErrors = validate(formData);
+        setIsSubmitting(true);
+        setErrors(validationErrors);
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+        setFormData((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    return { handleSubmit, handleChange, formData, errors, isSubmitting };
+};
+
+export default useAuthForm;
