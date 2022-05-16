012522603670d7ad431469a539c69484c3248f29
Gerardo <gr261212@gmail.com>
2022-05-15 21:15:46 -0400

    add vite type checker plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        checker({
+            typescript: true,
+        }),
+    ],
     server: {
         proxy: {
             "/api": "http://localhost:5000/",
