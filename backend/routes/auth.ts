194bc11332dfc81c340fc95c3cf12cd9087c6e4e
Gerardo <gr261212@gmail.com>
2022-05-16 12:06:11 -0400

    create auth routes

---
+++ backend/routes/auth.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { register, login, logout } from "../controller/auth";
+
+const router = express.Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/logout");
+
+export default router;
