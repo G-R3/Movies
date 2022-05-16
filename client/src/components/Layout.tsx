b6c62ae1145d8e627f5746c8d38075ddff371d09
Gerardo <gr261212@gmail.com>
2022-05-16 19:27:47 -0400

    redesign home page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,10 +3,9 @@ import { Box, Container, useColorModeValue } from "@chakra-ui/react";
 import Navbar from "./Navbar";
 
 const Layout = (): JSX.Element => {
-    const bg = useColorModeValue("gray.100", "gray.900");
     const color = useColorModeValue("gray.800", "gray.300");
     return (
-        <Box backgroundColor={bg} color={color} h="full" paddingX={[2, 10, 20]}>
+        <Box color={color} paddingX={[2, 10, 20]}>
             <Container maxW="container.xl" p={0}>
                 <header>
                     <Navbar />
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,20 +1,94 @@
-import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import {
+    Button,
+    Box,
+    Flex,
+    Grid,
+    GridItem,
+    Heading,
+    Text,
+    VStack,
+} from "@chakra-ui/react";
+import { CSSProperties } from "react";
+import { Link } from "react-router-dom";
 
 const Home = (): JSX.Element => {
     return (
-        <Flex
-            as="main"
-            direction="column"
-            alignItems="center"
-            justify="center"
-            h="300px"
-        >
-            <Heading fontSize="4xl">Movie Manager</Heading>
-            <Text>Track and organize your movies</Text>
-            <Button mt="5" colorScheme="purple">
-                Get Started
-            </Button>
-        </Flex>
+        <>
+            <Grid templateColumns="repeat(12, 1fr)" gap={6}>
+                <GridItem colSpan={6}>
+                    <Flex
+                        h="full"
+                        flexDirection={"column"}
+                        justifyContent={"center"}
+                    >
+                        <Heading fontSize="7xl">Movie Manager</Heading>
+                        <Text textAlign={"start"} fontSize={"xl"}>
+                            Track and organize your movies
+                        </Text>
+                        <Button
+                            mt="5"
+                            colorScheme="purple"
+                            alignSelf={"start"}
+                            fontSize={"lg"}
+                            fontWeight={"semibold"}
+                            padding={"6"}
+                        >
+                            <Link to="/browse">Get Started</Link>
+                        </Button>
+                    </Flex>
+                </GridItem>
+                <GridItem colSpan={6}>
+                    <VStack h="full" alignItems="start" gap={10} mt={"8"}>
+                        <Flex gap={10}>
+                            <Box
+                                w="400px"
+                                h="280px"
+                                bg="tomato"
+                                borderRadius={10}
+                                shadow={"2xl"}
+                                className={"img-place"}
+                                style={{ "--order": 1 } as CSSProperties}
+                            ></Box>
+                            <Box
+                                w="400px"
+                                h="280px"
+                                bg="tomato"
+                                borderRadius={10}
+                                shadow={"2xl"}
+                                className={"img-place"}
+                                style={{ "--order": 2 } as CSSProperties}
+                            ></Box>
+                        </Flex>
+                        <Flex gap={10}>
+                            <Box
+                                w="50"
+                                h="50"
+                                bg="tomato"
+                                borderRadius={10}
+                            ></Box>
+                            <Box
+                                w="400px"
+                                h="280px"
+                                bg="tomato"
+                                borderRadius={10}
+                                shadow={"2xl"}
+                                className={"img-place"}
+                                style={{ "--order": 3 } as CSSProperties}
+                            ></Box>
+                            <Box
+                                w="400px"
+                                h="280px"
+                                bg="tomato"
+                                borderRadius={10}
+                                shadow={"2xl"}
+                                className={"img-place"}
+                                style={{ "--order": 4 } as CSSProperties}
+                            ></Box>
+                        </Flex>
+                    </VStack>
+                </GridItem>
+            </Grid>
+        </>
     );
 };
 
