b47502f07a17fbedb2b87b471b146465bb7f3295
Gerardo <gr261212@gmail.com>
2022-05-20 14:27:59 -0400

    movies can be added to lists

--- backend/controller/list.ts
+++ backend/controller/list.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 import List from "../models/list";
+import Movie from "../models/Movie";
 
 const getUserLists = async (req: Request, res: Response) => {
     const { id } = req.user;
@@ -46,4 +47,53 @@ const createList = async (req: Request, res: Response) => {
     }
 };
 
-export { getUserLists, createList };
+const addMovieToList = async (req: Request, res: Response) => {
+    const { id, movie } = req.body;
+    let movieId: Types.ObjectId;
+
+    if (!id || !movie) {
+        return res
+            .status(400)
+            .send({ seuccess: false, message: "Missing movie or id" });
+    }
+
+    // check if movie already exists
+    const foundMovie = await Movie.findOne({ movieId: movie.id });
+
+    if (!foundMovie) {
+        const movieGenres = movie.genres.map(
+            (genre: { name: string; id: number }) => genre.name
+        );
+        ({ _id: movieId } = await Movie.create({
+            budget: movie.budget,
+            genres: movieGenres,
+            movieId: movie.id,
+            name: movie.name || movie.original_title,
+            overview: movie.overview,
+            popularity: movie.popularity,
+            releaseDate: movie.release_date,
+            runtime: movie.runtime,
+            tagline: movie.tagline,
+            voteAverage: movie.vote_average,
+            voteCount: movie.vote_count,
+        }));
+    } else {
+        movieId = foundMovie._id;
+    }
+
+    const list = await List.findById(id);
+    const movieInList = list?.movies.includes(movieId);
+
+    if (movieInList) {
+        return res
+            .status(400)
+            .send({ success: false, message: "Movie is already in this list" });
+    }
+
+    list?.movies.push(movieId);
+    await list?.save();
+
+    res.status(200).send({ success: true, message: "Movie added to list" });
+};
+
+export { getUserLists, createList, addMovieToList };
