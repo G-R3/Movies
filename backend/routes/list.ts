7fc66d206253960cfaa0732e4a9c20fc8c14b21e
Gerardo <gr261212@gmail.com>
2022-05-20 13:07:22 -0400

    create route to add movie to list

--- backend/routes/list.ts
+++ backend/routes/list.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { createList, getUserLists } from "../controller/list";
+import { createList, getUserLists, addMovieToList } from "../controller/list";
 import isAuthorized from "../middleware/auth";
 
 const router = express.Router();
 
 router.get("/lists", isAuthorized, getUserLists);
 router.post("/create", isAuthorized, createList);
-
+router.post("/add", isAuthorized, addMovieToList);
 export default router;
