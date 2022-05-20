37591fabd66662bdaee899ca8ff72f08343d6f64
Gerardo <gr261212@gmail.com>
2022-05-20 18:38:11 -0400

    check if logged in user is owner of list

---
+++ backend/middleware/isListOwner.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from "express";
+import List from "../models/list";
+
+const isListOwner = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { listId } = req.params;
+
+        const list = await List.findById(listId);
+
+        if (!list?.owner?.equals(req?.user?.id)) {
+            return res.status(401).send({
+                success: false,
+                message: "You do not have permission to delete list",
+            });
+        }
+
+        next();
+    } catch (err) {
+        return res
+            .status(401)
+            .send({ success: false, message: "Failed to delete list" });
+    }
+};
+
+export default isListOwner;
