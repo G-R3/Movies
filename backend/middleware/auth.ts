ad37ffa39b477eacda1a157a15b0f2fef3f3a39c
Gerardo <gr261212@gmail.com>
2022-05-20 18:41:30 -0400

    fix the 'req.user does not exist on type..' error i think

--- backend/middleware/auth.ts
+++ backend/middleware/auth.ts
@@ -1,6 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
+// https://stackoverflow.com/questions/58200432/argument-of-type-req-request-res-iresponse-next-nextfunction-void-is
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/46861
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
+    }
+}
 const isAuthorized = (req: Request, res: Response, next: NextFunction) => {
     try {
         const { token } = req.cookies;
