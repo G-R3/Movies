21f106ef6b4c4a4b8f7336b0a0b34430c1cb3a28
Gerardo <gr261212@gmail.com>
2022-05-20 22:38:50 -0400

    export ListPage

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,5 +2,6 @@ import Browse from "./Browse";
 import Home from "./Home";
 import MoviePage from "./MoviePage";
 import Profile from "./Profile";
+import ListPage from "./ListPage";
 
-export { Browse, Home, MoviePage, Profile };
+export { Browse, Home, MoviePage, Profile, ListPage };
