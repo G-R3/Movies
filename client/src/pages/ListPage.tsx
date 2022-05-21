40b38af5746de2d7c2aed45a29b36910b6e17e03
Gerardo <gr261212@gmail.com>
2022-05-20 22:50:20 -0400

    redirect to 404

---
+++ client/src/pages/ListPage.tsx
@@ -0,0 +1,64 @@
+import { Heading, Text, SimpleGrid } from "@chakra-ui/react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Card from "../components/Card";
+
+interface IList {
+    title: string;
+    description: string;
+    movies: [];
+    createdAt: string;
+}
+
+export default function ListPage() {
+    const { listId } = useParams();
+    const [list, setList] = useState<IList>();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getMovies = async () => {
+            try {
+                const response = await fetch(`/api/list/${listId}/movies`);
+
+                if (response.status === 404) {
+                    navigate("/404", { replace: true });
+                }
+
+                const data = await response.json();
+
+                if (!data.success) {
+                    navigate("/404", { replace: true });
+                }
+
+                setList(data.list);
+            } catch (err) {
+                console.warn(err);
+            }
+        };
+
+        getMovies();
+    }, []);
+
+    return (
+        <>
+            <Heading
+                fontSize={"7xl"}
+                marginTop={"28"}
+                marginBottom={"10"}
+                className="profile-header"
+            >
+                Watchlist
+            </Heading>
+            <Text fontSize={"xl"} color={"gray.400"}>
+                For movies that you just most watch
+            </Text>
+
+            <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={"40px"}>
+                {list &&
+                    list.movies.map((movie: any) => (
+                        <Card key={movie.id} movie={movie} />
+                    ))}
+            </SimpleGrid>
+        </>
+    );
+}
