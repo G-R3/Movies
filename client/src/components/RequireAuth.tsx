c7cd0936ea10f83bcacebfd2d4b7b6b792a5867b
Gerardo <gr261212@gmail.com>
2022-05-21 22:59:12 -0400

    wrapper component to protect routes

---
+++ client/src/components/RequireAuth.tsx
@@ -0,0 +1,13 @@
+import { useContext, ReactElement } from "react";
+import { Navigate } from "react-router-dom";
+import { AuthContext } from "../context/Auth";
+
+interface Props {
+    children: ReactElement;
+}
+
+export default function RequireAuth({ children }: Props): JSX.Element {
+    const { isLoggedIn } = useContext(AuthContext);
+    if (isLoggedIn === false) return <Navigate to="/" replace />;
+    return <>{children}</>;
+}
