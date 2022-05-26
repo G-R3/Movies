d8ee2e1c479d3fa9297364ea5d27522efa284131
Gerardo <gr261212@gmail.com>
2022-05-26 10:22:51 -0400

    create two context for list and dispatch

--- client/src/context/ListContext.tsx
+++ client/src/context/ListContext.tsx
@@ -7,16 +7,33 @@ interface Props {
 
 const initialState = {
     lists: [],
+    isLoading: true,
 };
 
 export const ListContext = createContext<any>(initialState);
+export const ListDispatchContext = createContext<any>(null);
 
 export const ListProvider = ({ children }: Props) => {
     const [state, dispatch] = useReducer(listsReducer, initialState);
 
+    useEffect(() => {
+        const getLists = async () => {
+            dispatch({ type: "FETCH_LIST" });
+
+            const response = await fetch("/api/lists");
+            const data = await response.json();
+
+            dispatch({ type: "SET_LIST", payload: data.lists });
+        };
+
+        getLists();
+    }, []);
+
     return (
-        <ListContext.Provider value={{ lists: state.lists, dispatch }}>
-            {children}
+        <ListContext.Provider value={state}>
+            <ListDispatchContext.Provider value={dispatch}>
+                {children}
+            </ListDispatchContext.Provider>
         </ListContext.Provider>
     );
 };
